4b71e135b602bfcccb3dd4dfba56d06126eb0acb
Shri12345patil <86612784+Shri12345patil@users.noreply.github.com>
2021-08-06 20:36:36 +0530

    Update App.js

--- 3-Function_Component/src/App.js
+++ 3-Function_Component/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function App() {
-  return <h2>Hi, this is function component!</h2>;
-}
+function App(props) {  
+  return <h1>Welcome to the , {props.name}</h1>;  
+}  
+
+//without props function component
+// function App() {
+//   return <h2>Hi, I am also a Car!</h2>;
+// }
 
 export default App;
