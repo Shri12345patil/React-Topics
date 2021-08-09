742269529abf726a34773330688d78be4ed2680f
Shri12345patil <jayshri.patil@sts.in>
2021-08-09 11:45:58 +0530

    usestate_hook(read more-read less hidden)

---
+++ 23-usestate_hook(read more-read less hidden)/src/App.js
@@ -0,0 +1,34 @@
+// First: import useState. It's a named export from 'react'
+// Or we could skip this step, and write React.useState
+import React, { useState } from 'react';
+// This component expects 2 props:
+//   text - the text to display
+//   maxLength - how many characters to show before "read more"
+function App({ text, maxLength }) {
+  // Create a piece of state, and initialize it to `true`
+  // `hidden` will hold the current value of the state,
+  // and `setHidden` will let us change it
+  const [hidden, setHidden] = useState(true);
+
+  // If the text is short enough, just render it
+  if (text.length <= maxLength) {
+    return <span>{text}</span>;
+  }
+
+  // Render the text (shortened or full-length) followed by
+  // a link to expand/collapse it.
+  // When a link is clicked, update the value of `hidden`,
+  // which will trigger a re-render
+  return (
+    <span>
+      {hidden ? `${text.substr(0, maxLength).trim()} ...` : text}
+      {hidden ? (
+        <a onClick={() => setHidden(false)}> read more</a>
+      ) : (
+        <a onClick={() => setHidden(true)}> read less</a>
+      )}
+    </span>
+  );
+}
+
+export default App;
---
+++ 23-usestate_hook(read more-read less hidden)/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <App
+    text={`Focused, hard work is the real key
+      to success. Keep your eyes on the goal, 
+      and just keep taking the next step 
+      towards completing it.`}
+    maxLength={35}
+  />,
+  document.querySelector('#root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
